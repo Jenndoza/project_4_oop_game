1a0ad7ba09f01c08555e1cf7ab11fd573899c678
Jenndoza <j.mendo1997@gmail.com>
2022-07-14 20:23:23 -0500

    Squiggly bracket causing an error on line 22

--- js/Game.js
+++ js/Game.js
@@ -2,4 +2,24 @@
  * Project 4 - OOP Game App
  * Game.js */
 
-console.log("hello")
+//Game class and properties.
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase("Not quite"),
+            new Phrase("Almost there"),
+            new Phrase("Keep trying"),
+            new Phrase("Super close"),
+            new Phrase("Icy hot")
+        ];
+        this.activePhrase = null;
+    } 
+}
+
+
+//This method selects and returns a random phrase from the array of phrases stored in the phrases property above.
+getRandomPhrase() {
+    const selectedPhrase = Math.floor(Math.random() * this.phrases.length);
+    return this.phrases[selectedPhrase];
+};
--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const phrase = new Phrase('Life is like a box of chocolates'); //Testing step 2 & 3
+
+
+const game = new Game(); //Testing step 4 
+game.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
